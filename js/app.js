d97ed57bebdde99d44562f318c1b5412e8bd644e
divanini <equiptrix@outlook.com>
2023-08-12 13:58:50 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const password = document.querySelector('.container__form-password')
+const checkbox = document.querySelector('input[type="checkbox"]');
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    password.type = 'text'
+  } else {
+    password.type = 'password'
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const loginButton = document.getElementById("loginButton");
+    loginButton.addEventListener("click", function() {
+        const usernameInput = document.querySelector(".container__form-name");
+        const passwordInput = document.querySelector(".container__form-password");
+        fetch("../user/data.json")
+            .then(response => response.json())
+            .then(data => {
+                const enteredUsername = usernameInput.value;
+                const enteredPassword = passwordInput.value;
+
+                const matchingUser = data.find(user => user.username === enteredUsername && user.password === enteredPassword);
+
+                if (matchingUser) {
+                    window.location.href = "home.html";
+                } else {
+                    location.reload()
+                }
+            })
+            .catch(error => console.error("Error fetching data.json:", error));
+    });
+});
